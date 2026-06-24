19354e72c2eb69710fc6f7233aa68d652561ac59
lowaxisa <lowaxisa@gmail.com>
2026-06-24 09:45:42 -0300

    removendo a renderização do client.js

--- client.js
+++ client.js
@@ -3,56 +3,10 @@ console.log("iniciando...");
 // --- imports
 import { dom } from './tools/dlink.js';
 import { send, recive } from './tools/api.js';
+import { show_page, show_task } from './tools/render.js';
 
 // --- global var
-const page_list = {
-	"page-init": dom.it.page,
-	"page-form": dom.fm.page,
-};
-
 const api = "https://script.google.com/macros/s/AKfycbxMx-8HRw3GNRX3acOmW4NZwYodxyLCbYjJcj5Er8nLnEQsfhy1KZt4gEl0LPLSnEqzEQ/exec";
-let task_count = 0
-
-// --- helpers
-function show_page(page, mode = "block") {
-	for (const [key, value] of Object.entries(page_list)) {
-		if (key === page) {
-			value.style.display = mode;
-		} else {
-			value.style.display = "none";
-		}
-	}
-}
-
-function show_task(target, title, desc, date) {
-	task_count++;
-
-	let task_div   = document.createElement("div");
-	let task_title = document.createElement("h2");
-	let task_desc  = document.createElement("p");
-	let task_date  = document.createElement("h3");
-
-	task_div.id   = `task${task_count}-div`;
-	task_title.id = `task${task_count}-title`;
-	task_desc.id  = `task${task_count}-desc`;
-	task_date.id  = `task${task_count}-date`;
-
-	task_title.innerText = `Trabalho: ${title}`;
-	task_desc.innerText  = `Descrição: ${desc}`;
-	task_date.innerText  = `Data de entrega: ${date}`;
-
-	task_div.appendChild(task_title);
-	task_div.appendChild(task_desc);
-	task_div.appendChild(task_date);
-
-	// style
-	task_div.style.backgroundColor = "#bfcade";
-	task_div.style.padding = "5px 20px";
-	task_div.style.borderRadius = "25px";
-	task_div.style.marginBottom = "15px";
-
-	target.appendChild(task_div);
-}
 
 // --- logic
 async function load_tasks() {
@@ -81,27 +35,6 @@ async function send_task(task_title, task_desc, task_date) {
 }
 
 // --- init
-show_page("none"); // hide everthing
-// defining styles
-dom.it.inewf.style.fontSize = "140px";
-dom.it.inewf.style.borderRadius = "50px";
-dom.it.inewf.style.padding = "0px 40px";
-
-dom.fm.isendf.style.fontSize = "120px";
-dom.fm.isendf.style.borderRadius = "50px";
-dom.fm.isendf.style.padding = "0px 40px";
-dom.fm.isendf.style.backgroundColor = "green";
-dom.fm.isendf.style.color = "white";
-
-dom.fm.icancelf.style.fontSize = "120px";
-dom.fm.icancelf.style.borderRadius = "50px";
-dom.fm.icancelf.style.padding = "0px 40px";
-dom.fm.icancelf.style.backgroundColor = "red";
-dom.fm.icancelf.style.color = "white";
-
-// setup page
-show_page("page-init");
-
 dom.it.inewf.addEventListener("click", () => {
 	show_page("page-form", "flex");
 });
---
+++ tools/render.js
@@ -0,0 +1,73 @@
+// --- imports
+import { dom } from './tools/dlink.js';
+
+// --- global var
+const page_list = {
+	"page-init": dom.it.page,
+	"page-form": dom.fm.page,
+};
+
+let task_count = 0
+
+// --- helpers
+export function show_page(page, mode = "block") {
+	for (const [key, value] of Object.entries(page_list)) {
+		if (key === page) {
+			value.style.display = mode;
+		} else {
+			value.style.display = "none";
+		}
+	}
+}
+
+export function show_task(target, title, desc, date) {
+	task_count++;
+
+	let task_div   = document.createElement("div");
+	let task_title = document.createElement("h2");
+	let task_desc  = document.createElement("p");
+	let task_date  = document.createElement("h3");
+
+	task_div.id   = `task${task_count}-div`;
+	task_title.id = `task${task_count}-title`;
+	task_desc.id  = `task${task_count}-desc`;
+	task_date.id  = `task${task_count}-date`;
+
+	task_title.innerText = `Trabalho: ${title}`;
+	task_desc.innerText  = `Descrição: ${desc}`;
+	task_date.innerText  = `Data de entrega: ${date}`;
+
+	task_div.appendChild(task_title);
+	task_div.appendChild(task_desc);
+	task_div.appendChild(task_date);
+
+	// style
+	task_div.style.backgroundColor = "#bfcade";
+	task_div.style.padding = "5px 20px";
+	task_div.style.borderRadius = "25px";
+	task_div.style.marginBottom = "15px";
+
+	target.appendChild(task_div);
+}
+
+// --- init
+show_page("none"); // hide everthing
+// defining styles
+dom.it.inewf.style.fontSize = "140px";
+dom.it.inewf.style.borderRadius = "50px";
+dom.it.inewf.style.padding = "0px 40px";
+
+dom.fm.isendf.style.fontSize = "120px";
+dom.fm.isendf.style.borderRadius = "50px";
+dom.fm.isendf.style.padding = "0px 40px";
+dom.fm.isendf.style.backgroundColor = "green";
+dom.fm.isendf.style.color = "white";
+
+dom.fm.icancelf.style.fontSize = "120px";
+dom.fm.icancelf.style.borderRadius = "50px";
+dom.fm.icancelf.style.padding = "0px 40px";
+dom.fm.icancelf.style.backgroundColor = "red";
+dom.fm.icancelf.style.color = "white";
+
+// setup page
+show_page("page-init");
